85bbd7cb3440371bcd98084568cbaf840cbbf636
Menna Omar <mennaomara36@gmail.com>
2025-07-14 15:43:57 +0300

    + flutter course tutorial + finish ass_nti_bank_account

--- bin/ass_nti/ass1_bank_account_simulation/bank_account_simulation.dart
+++ bin/ass_nti/ass1_bank_account_simulation/bank_account_simulation.dart
@@ -1,7 +1,46 @@
 import 'dart:io';
 
 void main() {
-  createdAccount();
+  final account = createdAccount();
+
+  String name = account['name'];
+  int accountNumber = account['accountNumber'];
+  String accountType = account['accountType'];
+  double balance = account['balance'];
+
+  bool exit = false;
+
+  while (!exit) {
+    showMenu();
+
+    int choice =getPositiveInt('Choose an option: ');
+
+    switch (choice) {
+      case 1:
+        balance = deposit(balance);
+        break;
+      case 2:
+        balance = withdraw(balance);
+        break;
+      case 3:
+        predictProfit(balance);
+        break;
+      case 4:
+        showSummary(name, accountNumber, accountType, balance);
+        break;
+      case 5:
+        print('Thank you for using the banking system. Goodbye!');
+        exit = true;
+        break;
+      default:
+        print('Invalid choice. Please try again.');
+    }
+  }
+}
+
+String prompt(String message) {
+  stdout.write(message);
+  return stdin.readLineSync() ?? '';
 }
 
 String capitalizedWords(String text) {
@@ -11,70 +50,108 @@ String capitalizedWords(String text) {
   }).join(' ');
 }
 
-void createdAccount() {
-  String name = '';
-  int accountNumber = 0;
-  String? accountType = '';
-  double? initialBalance = 0.0;
+String getValidText(String message) {
+  String input;
+  do {
+    input = prompt(message).trim();
+    if (input.isEmpty) print('Input Can\'t be empty');
+  } while (input.isEmpty);
+  return capitalizedWords(input);
+}
 
+int getPositiveInt(String message) {
+  int? value;
+  do {
+    value = int.tryParse(prompt(message));
+    if (value == null || value <= 0) print('Enter a positive number.');
+  } while (value == null || value <= 0);
+  return value;
+}
+
+double getPositiveDouble(String message) {
+  double? value;
+  do {
+    value = double.tryParse(prompt(message));
+    if (value == null || value < 0) print('Enter a valid number(0 or more).');
+  } while (value == null || value < 0);
+  return value;
+}
+
+Map<String, dynamic> createdAccount() {
   print('Welcome To The Bank Account Simulation!\n');
 
-  while (name.trim().isEmpty) {
-    stdout.write('Enter Your Name: ');
-    name = stdin.readLineSync()!;
-    if (name.trim().isEmpty) {
-      print('Name can\'t be empty. Please try again.');
+  String name = getValidText('Enter your name: ');
+  int accountNumber = getPositiveInt('Enter account number: ');
+
+  String type;
+  do {
+    type = prompt('Enter account type (Saving / Checking): ').toLowerCase();
+    if (type != 'saving' && type != 'checking') {
+      print('Invalid type. Please enter "Saving" or "checking".');
     }
-  }
-  name = capitalizedWords(name);
+  } while (type != 'saving' && type != 'checking');
 
-  bool validAccountNumber = false;
-  while (!validAccountNumber) {
-    stdout.write('Enter Account Number: ');
-    int? inputAccountNumber = int.tryParse(stdin.readLineSync()!);
-    if (inputAccountNumber != null && inputAccountNumber > 0) {
-      accountNumber = inputAccountNumber;
-      validAccountNumber = true;
-    } else {
-      print('Invalid Account Number. Please enter a positive number.');
-    }
-  }
-
-  bool validAccountType = false;
-  while (!validAccountType) {
-    stdout.write('Enter Account Type (Saving / Checking): ');
-    String accountTypeInput = stdin.readLineSync()!;
-    String formattedType = accountTypeInput.toLowerCase();
-
-    if (formattedType == 'saving' || formattedType == 'checking') {
-      accountType = capitalizedWords(formattedType);
-      validAccountType = true;
-    } else {
-      print('Invalid Account Type. Please enter "Saving" or "Checking".');
-    }
-  }
-
-  bool validInitialBalance = false;
-  while (!validInitialBalance) {
-    stdout.write('Enter Initial Balance: ');
-    double? inputInitialBalance = double.tryParse(stdin.readLineSync()!);
-
-    if (inputInitialBalance != null && inputInitialBalance >= 0) {
-      initialBalance = inputInitialBalance;
-      validInitialBalance = true;
-    } else {
-      print("Invalid Balance. Initial Balance must be 0 or more.");
-    }
-  }
+  double initialBalance = getPositiveDouble('Enter initial balance: ');
 
   print("\nAccount Created Successfully!");
   print("Welcome, $name! Your account is ready to use.\n");
-  print('==================== Account Summary ====================');
-  print('Name: $name');
-  print('Account Number: $accountNumber');
-  print('Account Type: $accountType');
-  print('Initial Balance: \$${initialBalance!.toStringAsFixed(2)}');
-  print('=========================================================');
 
+  return {
+    'name': name,
+    'accountNumber': accountNumber,
+    'accountType': capitalizedWords(type),
+    'balance': initialBalance
+  };
+}
 
+void showMenu() {
+  print('\n==================== Menu ====================');
+  print('1. Deposit');
+  print('2. Withdraw');
+  print('3. Predict Future Balance');
+  print('4. View Account Summary');
+  print('5. Exit');
+}
+
+void showSummary(
+    String name, int accountNumber, String accountType, double balance) {
+  print('\n========== Account Summary ==========');
+  print('Name           : $name');
+  print('Account Number : $accountNumber');
+  print('Account Type   : $accountType');
+  print('Balance        : \$${balance.toStringAsFixed(2)}');
+}
+
+double deposit(double balance) {
+  double amount = getPositiveDouble('Enter amount to deposit: ');
+
+  balance += amount;
+  print('Deposit successful. New balance: \$${balance.toStringAsFixed(2)}');
+
+  return balance;
+}
+
+double withdraw(double balance) {
+  double amount = getPositiveDouble('Enter amount to withdraw: ');
+
+  if (amount > balance) {
+    print('Insufficient balance. Withdrawal denied.');
+  } else {
+    balance -= amount;
+    print(
+        'Withdrawal successful. New balance: \$${balance.toStringAsFixed(2)}');
+  }
+  return balance;
+}
+
+void predictProfit(double balance) {
+  int years = getPositiveInt('Enter number of years: ');
+  double rate = getPositiveDouble('Enter annual profit rate (%):');
+
+  double futureBalance = balance * (1 + (rate / 100 * years));
+  int roundedBalance = futureBalance.round();
+
+  print(
+      'Predicted future balance after $years years: \$${futureBalance.toStringAsFixed(2)}');
+  print('Rounded balance: \$$roundedBalance');
 }
